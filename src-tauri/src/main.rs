1b8deeda7629954ead020dfed823c764e311f220
gtshadow33 <gtshadow33@gmail.com>
2025-11-06 16:13:15 +0100

    visual configurations

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,12 +1,18 @@
 #[tauri::command]
 fn compute(expression: String) -> Result<String, String> {
-let expr = expression.replace(" ", "");
+    let expr = expression.replace(" ", "");
 
-
-match meval::eval_str(expr) {
-Ok(v) => Ok(v.to_string()),
-Err(_) => Err("Error al calcular".to_string()),
-}
+    match meval::eval_str(expr) {
+        Ok(v) => {
+            // Si es entero, formatearlo sin decimales
+            if v.fract() == 0.0 {
+                Ok(format!("{}", v as i64))
+            } else {
+                Ok(format!("{:.3}", v)) // deja decimales naturales
+            }
+        }
+        Err(_) => Err("Error al calcular".to_string()),
+    }
 }
 
 
